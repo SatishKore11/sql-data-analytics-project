3bb6c40a40fba60983e8841839f3545fbfd124fc
Satish  Sampatrao Kore <154883166+SatishKore11@users.noreply.github.com>
2025-12-20 17:54:25 +0530

    Add part-to-whole analysis SQL script
    This SQL script performs a part-to-whole analysis to evaluate category contributions to overall sales, using aggregate and window functions.

---
+++ scripts/11_part_to_whole_analysis.sql
@@ -0,0 +1,32 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+-- Which categories contribute the most to overall sales?
+WITH category_sales_contribution AS (
+SELECT
+    p.category,
+    SUM(f.sales_amount) AS sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON f.product_key = p.product_key
+GROUP BY p.category
+)
+
+SELECT 
+    category,
+    sales,
+    SUM(sales) OVER() total_sales,
+    CONCAT(ROUND((CAST(sales AS FLOAT) / SUM(sales) OVER()) * 100, 2), '%') AS contribution
+FROM category_sales_contribution
+ORDER BY sales DESC;
